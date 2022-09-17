5e84b1d1df2c27219b607b4bad9cb0f0d861e114
Edward240402 <eduard.coar@gmail.com>
2022-09-16 23:01:42 -0500

    initial commit

---
+++ Go2Climb.API/Go2Climb.API/Activities/Resources/ActivityResource.cs
@@ -0,0 +1,13 @@
+﻿
+using Go2Climb.API.Services.Resources;
+
+namespace Go2Climb.API.Resources
+{
+    public class ActivityResource
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public ServiceResource Service;
+    }
+}
